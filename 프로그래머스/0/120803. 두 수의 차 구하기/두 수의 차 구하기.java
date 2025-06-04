9767e9f505fd1debc05ebf34b8d43cdc80f944c3
NYoungLEE <skdud0694@gmail.com>
2025-06-04 12:40:36 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.03 ms, Memory: 92.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        int answer = 0;
+        
+        boolean val = (-50000 <= num1 && num1 <= 50000) && (-50000 <= num2 && num2 <= 50000);
+        
+        if(val) answer = num1 - num2;
+        
+        return answer;
+    }
+}
