5806f8e51cdc3fddd30541c6565bdc1a2946bae5
NYoungLEE <skdud0694@gmail.com>
2025-06-19 23:45:19 +0900

    [level 0] Title: 모스부호 (1), Time: 2.01 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String letter) {
+        
+        String answer = "";
+        String[] morse = { 
+            ".-", "-...", "-.-.", "-..", ".", "..-.",
+            "--.", "....", "..", ".---", "-.-", ".-..",
+            "--", "-.", "---", ".--.", "--.-", ".-.",
+            "...", "-", "..-", "...-", ".--", "-..-",
+            "-.--", "--.."
+        };
+        
+        String[] arr = letter.split(" ");
+        
+        for(String s : arr) {
+            for(int i = 0; i < morse.length; i++) {
+                if(s.equals(morse[i])) answer += Character.toString(i + 'a');
+            }
+        }
+        return answer;
+    }
+}
