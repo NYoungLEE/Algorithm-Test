c19d074876156a2381f3e7fbccb314a2cd623926
NYoungLEE <skdud0694@gmail.com>
2025-07-06 22:26:13 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] array) {
+        
+        int[] answer = new int[2];
+        int max = array[0];
+        int count = 0;
+        
+        for(int i = 0; i < array.length; i++) {
+            max = array[i] > max ? array[i] : max;
+            if(max == array[i]) count = i;
+        }
+        
+        answer[0] = max;
+        answer[1] = count;
+        
+        return answer;
+    }
+}
