6f4a329c091acb60e871997a7930674c0ac6603c
NYoungLEE <skdud0694@gmail.com>
2025-10-15 23:50:33 +0900

    [Silver I] Title: 절댓값 힙, Time: 1016 ms, Memory: 105824 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();   // 질의 요청 개수
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+            int first_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
+            if(first_abs == second_abs) {
+                return o1 > o2 ? 1 : -1;
+            } else {
+                return first_abs - second_abs;
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            int request = sc.nextInt();
+            if(request == 0) {
+                if(queue.isEmpty()) {
+                    System.out.println("0");
+                } else {
+                    System.out.println(queue.poll());
+                }
+            } else {
+                queue.add(request);
+            }
+        }
+    }
+}
