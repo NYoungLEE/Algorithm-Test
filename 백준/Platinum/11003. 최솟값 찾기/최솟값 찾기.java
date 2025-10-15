2853b11787da2ecde40a48df17bf424a513c3dcf
NYoungLEE <skdud0694@gmail.com>
2025-10-15 20:16:02 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2288 ms, Memory: 619036 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());   // 숫자의 개수
+        int L = Integer.parseInt(st.nextToken());   // 슬라이딩 윈도우의 크기
+        Deque<Node> mydeque = new LinkedList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            int now = Integer.parseInt(st.nextToken());
+
+            while(!mydeque.isEmpty() && mydeque.getLast().value > now) {
+                mydeque.removeLast();
+            }
+            mydeque.addLast(new Node(now, i));
+
+            if(mydeque.getFirst().index <= i - L) {
+                mydeque.removeFirst();
+            }
+
+            bw.write(mydeque.getFirst().value + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    static class Node {
+
+        public int value;
+        public int index;
+
+        Node(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+    }
+}
