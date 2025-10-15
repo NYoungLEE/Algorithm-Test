e11f77def35fed2c19651f6d4cf1a0aa9b3aa0b9
NYoungLEE <skdud0694@gmail.com>
2025-10-15 22:43:37 +0900

    [Gold IV] Title: 오큰수, Time: 928 ms, Memory: 150812 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());  // 수열 개수
+        int[] A = new int[N];   // 수열 배열
+        int[] ans = new int[N]; // 정답 배열
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for(int i = 1; i < N; i++){
+            while(!stack.isEmpty() && A[stack.peek()] < A[i]) {
+                ans[stack.pop()] = A[i];
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()){
+            ans[stack.pop()] = -1;
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i = 0; i < N; i++){
+            bw.write(ans[i] + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
