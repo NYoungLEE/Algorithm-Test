a17219d637eccc17335bf959852735ef619d9993
NYoungLEE <skdud0694@gmail.com>
2025-10-19 11:06:03 +0900

    [Gold V] Title: 신기한 소수, Time: 760 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int N;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();   // 자릿수
+        DFS(2, 1);
+        DFS(3, 1);
+        DFS(5, 1);
+        DFS(7, 1);
+    }
+
+    private static void DFS(int number, int jarisu) {
+        if(jarisu == N) {
+            if(isPrime(number)) {
+                System.out.println(number);
+            }
+            return;
+        }
+        for(int i = 1; i < 10; i += 2) {
+            if (isPrime(number * 10 + i)) {
+                DFS(number * 10 + i, jarisu + 1);
+            }
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
