c110ca07a2d76e8cb7c45899a14f602a6b46c728
NYoungLEE <skdud0694@gmail.com>
2026-01-06 10:35:48 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 396 ms, Memory: 62604 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt(); // 조건 수
+    int[] A = new int[10000001];
+    for (int i = 2; i < A.length; i++) {
+      A[i] = i;
+    }
+    for (int i = 2; i <= Math.sqrt(A.length); i++) {
+      if (A[i] == 0) continue;
+      for (int j = 2 * i; j < A.length; j = j + i) {
+        A[j] = 0;
+      }
+    }
+    int i = N;
+    while (true) {
+      if (A[i] != 0) {
+        int result = A[i];
+        if (isPalindrome(result)) {
+          System.out.println(result);
+          break;
+        }
+      }
+      i++;
+    }
+  }
+
+  private static boolean isPalindrome(int num) {
+    char[] temp = String.valueOf(num).toCharArray();
+    int s = 0;
+    int e = temp.length - 1;
+    while (s < e) {
+      if (temp[s] != temp[e]) return false;
+      s++;
+      e--;
+    }
+    return true;
+  }
+}
