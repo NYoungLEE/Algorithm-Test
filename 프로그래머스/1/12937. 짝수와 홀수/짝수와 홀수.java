2e323e98f6765ae112c5591b0476b8f15579a532
NYoungLEE <skdud0694@gmail.com>
2026-03-22 23:22:37 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(int num) {
-        
-        String answer = (num % 2 == 0) ? "Even" : "Odd";
+        String answer = num % 2 == 0 ? "Even" : "Odd";
         return answer;
     }
 }
