385b15ee7568cd91df270a95adadb4e6776ad0a1
NYoungLEE <skdud0694@gmail.com>
2026-03-23 22:33:53 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -4,9 +4,10 @@ class Solution {
         int answer = 0;
         
         for (int i = 0; i < absolutes.length; i++) {
-            answer += (signs[i] == true) ? absolutes[i] : -absolutes[i];
+            if (signs[i]) answer += absolutes[i];
+            else answer -= absolutes[i];
         }
-        
+
         return answer;
     }
 }
