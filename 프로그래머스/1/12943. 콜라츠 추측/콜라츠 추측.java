08f01b1b91b3eceef115d0a70ef11f57bd7840c5
NYoungLEE <skdud0694@gmail.com>
2026-03-24 12:20:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -3,12 +3,13 @@ class Solution {
         
         long n = (long) num;
         
-        for(int i = 0; i < 500; i++) {
+        for (int i = 0; i < 500; i++) {
             
             if (n == 1) return i;
+            
             n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
         }
-
+        
         return -1;
     }
 }
