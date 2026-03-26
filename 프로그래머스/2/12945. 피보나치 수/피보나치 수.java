a07aff4236dfb86c938a242e87eeed7fc2ef1ee0
NYoungLEE <skdud0694@gmail.com>
2026-03-26 20:57:59 +0900

    [level 2] Title: 피보나치 수, Time: 2.31 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        
+        int[] num = new int[n + 1];
+        num[0] = 0; num[1] = 1;
+        
+        if (n >= 2) {
+            for (int i = 2; i <= n; i++) {
+                num[i] = (num[i - 1] + num[i - 2]) % 1234567;
+            }
+        }
+        
+        return num[n];
+    }
+}
