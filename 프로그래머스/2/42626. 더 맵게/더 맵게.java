9c7eeced607758dea751eb91b7cf61816bbef311
NYoungLEE <skdud0694@gmail.com>
2026-04-02 22:24:10 +0900

    [level 2] Title: 더 맵게, Time: 1368.03 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int num : scoville) {
+            pq.offer(num);
+        }
+        
+        while (pq.peek() < K) {
+            if (pq.size() == 1) return -1;
+            
+            int num1 = pq.poll();
+            int num2 = pq.poll();
+            
+            pq.offer(num1 + num2 * 2);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
