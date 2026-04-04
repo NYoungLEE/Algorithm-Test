da2c20aef7813a60e3a0ec161c48ed118e1bf98a
NYoungLEE <skdud0694@gmail.com>
2026-04-04 16:49:09 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 71.01 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] gems) {
+                
+        Set<String> set = new HashSet<>(Arrays.asList(gems));
+        int kind = set.size();
+        
+        Map<String, Integer> map = new HashMap<>();
+        int left = 0; int min = Integer.MAX_VALUE;
+        int[] answer = new int[2];
+        
+        for (int right = 0; right < gems.length; right++) {
+            map.put(gems[right], map.getOrDefault(gems[right], 0) + 1);
+            
+            while (map.size() == kind) {
+                if (min > right - left) {
+                    min = right - left;
+                    answer[0] = left + 1;
+                    answer[1] = right + 1;
+                }
+                
+                map.put(gems[left], map.get(gems[left]) - 1);
+                if (map.get(gems[left]) == 0) map.remove(gems[left]);
+                left++;
+            }
+        }
+        
+        return answer;
+    }
+}
