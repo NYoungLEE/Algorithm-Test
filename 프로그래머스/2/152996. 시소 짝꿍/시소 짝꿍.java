031db9e0bf5d0c1631d416921cc9d59ff9fcba42
NYoungLEE <skdud0694@gmail.com>
2026-04-20 14:30:54 +0900

    [level 2] Title: 시소 짝꿍, Time: 69.45 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int[] weights) {
+        
+        Arrays.sort(weights);
+        
+        long answer = 0;
+        Map<Integer, Long> map = new HashMap<>();
+        
+        for (int w : weights) {
+            answer += map.getOrDefault(w, 0L);
+            
+            if (w * 2 % 3 == 0) {
+                answer += map.getOrDefault(w * 2 / 3, 0L);
+            }
+            if (w % 2 == 0) {
+                answer += map.getOrDefault(w / 2, 0L);
+            }
+            if (w * 3 % 4 == 0) {
+                answer += map.getOrDefault(w * 3 / 4, 0L);
+            }
+            
+            map.put(w, map.getOrDefault(w, 0L) + 1);
+        }
+        
+        return answer;
+    }
+}
