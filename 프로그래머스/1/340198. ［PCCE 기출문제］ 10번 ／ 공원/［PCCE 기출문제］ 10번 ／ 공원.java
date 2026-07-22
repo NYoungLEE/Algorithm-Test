98ffec907f5a0674c33624cd3e43e8cb8b911f52
NYoungLEE <skdud0694@gmail.com>
2026-07-22 18:45:20 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 3.24 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -5,24 +5,14 @@ class Solution {
         
         Arrays.sort(mats);
         for (int i = mats.length - 1; i >= 0; i--) {
-            int size = mats[i];
-            for (int j = 0; j <= park.length - size; j++) {
-                for (int k = 0; k <= park[j].length - size; k++) {
-                    if(canPlace(park, j, k, size)) return mats[i];
+            for (int i = 0; i < park.length; i++) {
+                for (int j = 0; j < park.length; j++) {
+                    
                 }
             }
         }
         
-        return -1;
-    }
-    
-    private boolean canPlace(String[][] park, int x, int y, int size) {
-        for (int i = x; i < x + size; i++) {
-            for (int j = y; j < y + size; j++) {
-                if(!park[i][j].equals("-1")) return false;
-            }
-        }
-        
-        return true;
+        int answer = 0;
+        return answer;
     }
 }
