3c62e813b8ddf357919c299a825d0d398f66ab65
NYoungLEE <skdud0694@gmail.com>
2026-07-22 19:43:22 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 12.28 ms, Memory: 57.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(String s) {
+        
+        int answer = 1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            // 홀수 길이
+            answer = Math.max(answer, expand(s, i, i));
+            
+            // 짝수 길이
+            answer = Math.max(answer, expand(s, i, i + 1));
+        }
+
+        return answer;
+    }
+    
+    private int expand(String s, int left, int right) {
+        
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        
+        return right - left - 1;
+    }
+}
