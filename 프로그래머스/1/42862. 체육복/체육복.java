c01feda0e3dc24ad53309ae97f82c7e171e9e8cd
NYoungLEE <skdud0694@gmail.com>
2026-07-25 18:33:50 +0900

    [level 1] Title: 체육복, Time: 2.56 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,23 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Set<Integer> lostSet = new HashSet<>();
-        for (int num : lost) lostSet.add(num);
-        Set<Integer> reserveSet = new HashSet<>();
-        for (int num : reserve) reserveSet.add(num);
-        Set<Integer> intersectSet = new HashSet<>(lostSet);
-        intersectSet.retainAll(reserveSet);
-        lostSet.removeAll(intersectSet);
-        reserveSet.removeAll(intersectSet);
         
-        for (int r : reserveSet) {
-            if (lostSet.contains(r - 1)) {
-                lostSet.remove(r - 1);
-            } else if (lostSet.contains(r + 1)) {
-                lostSet.remove(r + 1);
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        int answer = n - lost.length;
+        
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    answer++;
+                    break;
+                }
             }
         }
         
-        return n - lostSet.size();
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+                    answer++;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
     }
 }
