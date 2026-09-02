6e9d934d9f1fc83b52becdd0c801a1ea128643ed
NYoungLEE <skdud0694@gmail.com>
2026-09-02 11:16:04 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.04 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int solution(int n, int w, int num) {
+        
+        int height = n / w + 1;
+        int[][] arr = new int[height][w];
+        
+        int count = 1;
+        int col = 0; int row = 0;
+        for (int i = 0; i < height; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < w; j++) {
+                    if (count > n) break;
+                    
+                    if (count == num) {
+                        col = i;
+                        row = j;
+                    }
+                    
+                    arr[i][j] = count++;
+                }
+            } else {
+                for (int j = w - 1; j >= 0; j--) {
+                    if (count > n) break;
+                    
+                    if (count == num) {
+                        col = i;
+                        row = j;
+                    }
+                    
+                    arr[i][j] = count++;
+                }
+            }
+        }
+        
+        int answer = 0;
+        for (int i = col; i < height; i++) {
+            if (arr[i][row] == 0) break;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
